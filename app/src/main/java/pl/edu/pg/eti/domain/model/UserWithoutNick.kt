7e4e13de07870a427f5cf64bbf5c1607536d602d
Creero <kenioo4167@gmail.com>
2022-11-17 18:31:55 +0100

    Updated Login request

--- app/src/main/java/pl/edu/pg/eti/domain/model/UserWithoutNick.kt
+++ app/src/main/java/pl/edu/pg/eti/domain/model/UserWithoutNick.kt
@@ -3,7 +3,7 @@ package pl.edu.pg.eti.domain.model
 import com.google.gson.annotations.SerializedName
 
 data class UserWithoutNick(
-    @SerializedName("username") val email: String,
+    @SerializedName("email") val email: String,
     @SerializedName("password") val password: String,
 ) {
 }
